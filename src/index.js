5c421a65cd4d48b277d868e94d4262dc76004654
Argynbek <argyyyn@gmail.com>
2023-09-18 11:25:12 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const el = <h1>This is how we dod</h1>
+const ToDoList = () => {
+  return <ul>
+      <li>this is how we do</li>
+    </ul>
+
+}
+
+const el = (
+  <div>
+    <h1>ToDo list</h1>
+    <input type="text" placeholder="search"/>
+    <ul>
+      <li>learn React</li>
+      <li>learn TS</li>
+      <ToDoList/>
+    </ul>
+  </div>
+)
+
+console.log(el)
+console.log(ToDoList())
 
 ReactDOM.render(el,document.getElementById('root'))
